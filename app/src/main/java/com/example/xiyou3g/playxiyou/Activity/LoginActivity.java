651efbc8d7236f4165debfef21bc8bbfd4d2c8b0
LanceShu <869924650@qq.com>
2018-04-20 10:39:52 +0800

    modified LoginActivity code

--- app/src/main/java/com/example/xiyou3g/playxiyou/Activity/LoginActivity.java
+++ app/src/main/java/com/example/xiyou3g/playxiyou/Activity/LoginActivity.java
@@ -54,6 +54,10 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 import static com.example.xiyou3g.playxiyou.Content.EduContent.*;
 import static com.example.xiyou3g.playxiyou.Content.AttenContent.*;
 
@@ -62,21 +66,37 @@ import static com.example.xiyou3g.playxiyou.Content.AttenContent.*;
  * on 2017/7/12.
  */
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity{
 
-    private EditText username;          //账号；
-    private EditText password;          //密码；
-    private EditText codeCheck;         //验证码；
-    private ImageView userClear;
-    private ImageView passSee;
-    private ImageView codeImage;
-    private Button login;
-    private Button tools;
+    @BindView(R.id.username)
+    EditText username;          //账号；
+
+    @BindView(R.id.password)
+    EditText password;          //密码；
+
+    @BindView(R.id.codeedit)
+    EditText codeCheck;         //验证码；
+
+    @BindView(R.id.userclear)
+    ImageView userClear;
+
+    @BindView(R.id.passSee)
+    ImageView passSee;
+
+    @BindView(R.id.codeCheck)
+    ImageView codeImage;
+
+    @BindView(R.id.login)
+    Button login;
+
+    @BindView(R.id.tools)
+    Button tools;
+
+    @BindView(R.id.remember_pass)
+    CheckBox remberPass;
 
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
-    private CheckBox remberPass;
-
     private Boolean isCanSee = false;
     private int flag;
     private byte[] imagebytes;
@@ -87,26 +107,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
-
-        islogin = 0;
-        cStuname = "";
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        setContentView(R.layout.login_activity);
+        ButterKnife.bind(this);
+        islogin = 0;
+        cStuname = "";
         mqueue = Volley.newRequestQueue(this);
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         initWight();                    //实例化控件;
-
         loginHanlder = new LoginHanlder(pref.edit(), this);
     }
 
     static class LoginHanlder extends Handler {
         private WeakReference<LoginActivity> activity;
         private WeakReference<SharedPreferences.Editor> editor;
-
-        LoginHanlder (LoginActivity activity) {
-            this.activity = new WeakReference<>(activity);
-        }
 
         LoginHanlder (SharedPreferences.Editor editor, LoginActivity activity) {
             this.activity = new WeakReference<>(activity);
@@ -129,17 +144,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void initWight() {
-        username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
-        codeCheck = (EditText) findViewById(R.id.codeedit);
-        userClear = (ImageView) findViewById(R.id.userclear);
-        passSee = (ImageView) findViewById(R.id.passSee);
-        codeImage = (ImageView) findViewById(R.id.codeCheck);
-        login = (Button) findViewById(R.id.login);
-        tools = (Button) findViewById(R.id.tools);
         codeCheck.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-        remberPass = (CheckBox) findViewById(R.id.remember_pass);
-
         boolean isRemem = pref.getBoolean("remember",false);
         if(isRemem){
             username.setText(pref.getString("username",""));
@@ -147,13 +152,157 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             remberPass.setChecked(true);
             cStuname = pref.getString("username","");
         }
-
         initCodeImage();
-        userClear.setOnClickListener(this);
-        passSee.setOnClickListener(this);
-        login.setOnClickListener(this);
-        tools.setOnClickListener(this);
-        codeImage.setOnClickListener(this);
+    }
+
+    @OnClick(R.id.userclear)
+    void userClear() {
+        username.setText("");
+        password.setText("");
+    }
+
+    @OnClick(R.id.passSee)
+    void passSee() {
+        if(isCanSee){
+            passSee.setImageResource(R.mipmap.eye_cant_see);
+            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        }else{
+            passSee.setImageResource(R.mipmap.eyes_can_see);
+            password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+        }
+        isCanSee = !isCanSee;
+    }
+
+    @OnClick(R.id.login)
+    void login() {
+//        if(cStuname.equals(username.getText().toString())){
+//            isCache = true;
+//        }else{
+//            isCache = false;
+//        }
+
+        final String url = "http://222.24.62.120/default2.aspx";
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("信息提示：");
+
+        loginName = username.getText().toString();
+        loginPassword = password.getText().toString();
+        loginCheckCode = codeCheck.getText().toString();
+
+        Log.e("information:", loginName + "-" + loginPassword + "-" + loginCheckCode);
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                if(flag == 1){
+                    Document document1 = Jsoup.parse(s);
+                    student_name = document1.getElementById("xhxm").text();
+                    student_name = student_name.substring(0, student_name.length() - 2);
+                    Log.e("student_name:", student_name);
+
+                    new GetPerInfo();                           //获取个人信息;
+                    getCurrentCourse();                         //获取当前课表;
+
+                    builder.setMessage("欢迎您，"+student_name+"同学！");
+                    builder.setPositiveButton("继续", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            editor = pref.edit();
+                            Log.e("checkbox state",remberPass.isChecked()+"");
+                            if(remberPass.isChecked()){
+                                editor.putBoolean("remember",true);
+                                editor.putString("username",username.getText().toString());
+                                editor.putString("password",password.getText().toString());
+                            }else{
+                                editor.clear();
+                            }
+                            editor.apply();
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }).create().show();
+                } else {
+                    builder.setMessage("登录失败，请重新登录");
+                    builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            initCodeImage();
+                        }
+                    }).create().show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                builder.setMessage("请检查网络连接");
+                builder.setPositiveButton("确认",null).create().show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> header = new HashMap<>();
+                header.put("Cookie",cookies);
+                header.put("Referer","http://222.24.62.120/");
+                header.put("Pragma","no-cache");
+                header.put("Accept-Encoding","gzip, deflate");
+                header.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                header.put("Accept-Language","zh-Hans-CN,zh-Hans;q=0.8");
+                header.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
+                Log.e("cookie",cookies+"1111111");
+                return header;
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return ("__VIEWSTATE=dDwxNTMxMDk5Mzc0Ozs%2BlYSKnsl%2FmKGQ7CKkWFJpv0btUa8%3D&txtUserName=" +loginName
+                        +"&Textbox1=&TextBox2=" +loginPassword
+                        +"&txtSecretCode=" +loginCheckCode
+                        +"&RadioButtonList1=%D1%A7%C9%FA&Button1=&lbLanguage=&hidPdrs=&hidsc=").getBytes();
+            }
+
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                System.out.println(response.headers);
+                Log.e("statusCode",response.statusCode+"   length="+response.data.length);
+                if(response.data.length > 7000){
+                    flag = 1;
+                }
+                return super.parseNetworkResponse(response);
+            }
+        };
+        mqueue.add(request);
+    }
+
+    @OnClick(R.id.tools)
+    void tools() {
+        PopupMenu popupMenu = new PopupMenu(this,tools);
+        popupMenu.getMenuInflater().inflate(R.menu.login_tools,popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.attend:
+                        Intent attIntent = new Intent(LoginActivity.this,AttenLoginActivity.class);
+                        startActivity(attIntent,ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+                        break;
+                    case R.id.siliu:
+                        Intent intent1 = new Intent(LoginActivity.this, SiliuActivitySe.class);
+                        startActivity(intent1, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
+                        break;
+                    case R.id.guide:
+                        Intent intent = new Intent(LoginActivity.this,GuideActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
+
+    @OnClick(R.id.codeCheck)
+    void codeCheck() {
+        initCodeImage();
     }
 
     private void initCodeImage() {
@@ -187,153 +336,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
         mqueue.add(getCheckCode);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.userclear:
-                username.setText("");
-                password.setText("");
-                break;
-            case R.id.passSee:
-                if(isCanSee){
-                    passSee.setImageResource(R.mipmap.eye_cant_see);
-                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                }else{
-                    passSee.setImageResource(R.mipmap.eyes_can_see);
-                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                }
-                isCanSee = !isCanSee;
-                break;
-            case R.id.codeCheck:
-                initCodeImage();
-                break;
-            case R.id.login:
-
-//                if(cStuname.equals(username.getText().toString())){
-//                    isCache = true;
-//                }else{
-//                    isCache = false;
-//                }
-
-                final String url = "http://222.24.62.120/default2.aspx";
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("信息提示：");
-
-                loginName = username.getText().toString();
-                loginPassword = password.getText().toString();
-                loginCheckCode = codeCheck.getText().toString();
-
-                Log.e("information:", loginName + "-" + loginPassword + "-" + loginCheckCode);
-                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String s) {
-                        if(flag == 1){
-                            Document document1 = Jsoup.parse(s);
-                            student_name = document1.getElementById("xhxm").text();
-                            student_name = student_name.substring(0, student_name.length() - 2);
-                            Log.e("student_name:", student_name);
-
-                            new GetPerInfo();                           //获取个人信息;
-                            getCurrentCourse();                         //获取当前课表;
-
-                            builder.setMessage("欢迎您，"+student_name+"同学！");
-                            builder.setPositiveButton("继续", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    editor = pref.edit();
-                                    Log.e("checkbox state",remberPass.isChecked()+"");
-                                    if(remberPass.isChecked()){
-                                        editor.putBoolean("remember",true);
-                                        editor.putString("username",username.getText().toString());
-                                        editor.putString("password",password.getText().toString());
-                                    }else{
-                                        editor.clear();
-                                    }
-                                    editor.apply();
-                                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }).create().show();
-                        } else {
-                            builder.setMessage("登录失败，请重新登录");
-                            builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    initCodeImage();
-                                }
-                            }).create().show();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError volleyError) {
-                        builder.setMessage("请检查网络连接");
-                        builder.setPositiveButton("确认",null).create().show();
-                    }
-                }){
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        Map<String,String> header = new HashMap<>();
-                        header.put("Cookie",cookies);
-                        header.put("Referer","http://222.24.62.120/");
-                        header.put("Pragma","no-cache");
-                        header.put("Accept-Encoding","gzip, deflate");
-                        header.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-                        header.put("Accept-Language","zh-Hans-CN,zh-Hans;q=0.8");
-                        header.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36");
-                        Log.e("cookie",cookies+"1111111");
-                        return header;
-                    }
-
-                    @Override
-                    public byte[] getBody() throws AuthFailureError {
-                        return ("__VIEWSTATE=dDwxNTMxMDk5Mzc0Ozs%2BlYSKnsl%2FmKGQ7CKkWFJpv0btUa8%3D&txtUserName=" +loginName
-                                +"&Textbox1=&TextBox2=" +loginPassword
-                                +"&txtSecretCode=" +loginCheckCode
-                                +"&RadioButtonList1=%D1%A7%C9%FA&Button1=&lbLanguage=&hidPdrs=&hidsc=").getBytes();
-                    }
-
-                    @Override
-                    protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                        System.out.println(response.headers);
-                        Log.e("statusCode",response.statusCode+"   length="+response.data.length);
-                        if(response.data.length > 7000){
-                            flag = 1;
-                        }
-                        return super.parseNetworkResponse(response);
-                    }
-                };
-                mqueue.add(request);
-                break;
-            case R.id.tools:
-                PopupMenu popupMenu = new PopupMenu(this,tools);
-                popupMenu.getMenuInflater().inflate(R.menu.login_tools,popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        switch (item.getItemId()){
-                            case R.id.attend:
-                                Intent attIntent = new Intent(LoginActivity.this,AttenLoginActivity.class);
-                                startActivity(attIntent,ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
-                                break;
-                            case R.id.siliu:
-                                Intent intent1 = new Intent(LoginActivity.this, SiliuActivitySe.class);
-                                startActivity(intent1, ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this).toBundle());
-                                break;
-                            case R.id.guide:
-                                Intent intent = new Intent(LoginActivity.this,GuideActivity.class);
-                                startActivity(intent);
-                                break;
-                        }
-                        return true;
-                    }
-                });
-                popupMenu.show();
-                break;
-        }
     }
 
     private void getCurrentCourse() {
